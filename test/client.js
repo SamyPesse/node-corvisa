825000bf29327c112818b6b13a03a4f5aa9dcec9
Samy Pessé <samypesse@gmail.com>
2015-04-15 13:41:25 -0500

    Normalize error from api and add test for it

--- test/client.js
+++ test/client.js
@@ -1,4 +1,22 @@
+var expect = require("chai").expect;
+var Q = require("q");
+var Corvisa = require("../");
 
 describe('Client', function() {
+
+    it('should return a 4xx code', function() {
+        var corvisa = new Corvisa("test");
+
+        return corvisa.sendSMS("+15554447001", {
+            to: "+14145554444",
+            text: "Hello World"
+        })
+        .then(function() {
+            throw "Hum, should fail";
+        }, function(e) {
+            expect(e.code).to.equal(401);
+            return Q();
+        });
+    });
 
 });
